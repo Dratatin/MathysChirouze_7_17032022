452cd81018c4197dd226c3e0047811e825a91552
Dratatin <matchirouze@orange.fr>
2022-03-31 18:59:49 +0200

    First algo steps

--- script/algo.js
+++ script/algo.js
@@ -1,5 +1,19 @@
+import { recipes } from "../data/recipes.js";
 import { Recipes } from "./recipes.js";
 
-export function algo () {
-    console.log("yo");
+export function algo (e) {
+    if (e.target.value.length > 2 ) {
+        const inputData = e.target.value;
+        const newTab = []
+        recipes.forEach(element => {
+            const contains = element.name.includes(inputData);
+            if (contains == true) {
+                newTab.push(element);
+            }
+        });
+        new Recipes (newTab);
+    }
+    else {
+        new Recipes (recipes);
+    }
 }
