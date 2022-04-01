2c2686aa00c500a4daa7545dda1c6edfa5903abe
Dratatin <matchirouze@orange.fr>
2022-04-01 13:25:55 +0200

    search by ingredients, title, description

--- script/algo.js
+++ script/algo.js
@@ -3,15 +3,38 @@ import { Recipes } from "./recipes.js";
 
 export function algo (e) {
     if (e.target.value.length > 2 ) {
-        const inputData = e.target.value;
-        const newTab = []
+        const inputData = e.target.value.toLowerCase();
+        const newTabRecipes = []
         recipes.forEach(element => {
-            const contains = element.name.includes(inputData);
-            if (contains == true) {
-                newTab.push(element);
+
+            //looking for a match
+            let findIt = false;
+            function search () {
+                const findInTitle = element.name.toLowerCase().includes(inputData);
+                const findInDescription = element.description.toLowerCase().includes(inputData);
+                let findInIngredients = false;
+
+                element.ingredients.forEach(element => {
+                    const see = element.ingredient.toLowerCase().includes(inputData);
+                    if (see == true) {
+                        findInIngredients = true
+                    }
+                });
+
+                if (findInTitle || findInDescription || findInIngredients == true) {
+                    findIt = true;
+                }
+                else {
+                    findIt = false;
+                }
+            }
+            search();
+
+            if (findIt == true) {
+                newTabRecipes.push(element);
             }
         });
-        new Recipes (newTab);
+        new Recipes (newTabRecipes);
     }
     else {
         new Recipes (recipes);
