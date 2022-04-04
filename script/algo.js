d36e52ce864af45a4276116604cca39abf52d9ba
Dratatin <matchirouze@orange.fr>
2022-04-04 13:09:15 +0200

    simplify algo rewrite

--- script/algo.js
+++ script/algo.js
@@ -4,34 +4,10 @@ import { Recipes } from "./recipes.js";
 export function algo (e) {
     if (e.target.value.length > 2 ) {
         const inputData = e.target.value.toLowerCase();
-        const newTabRecipes = []
-        recipes.forEach(element => {
-
-            //looking for a match
-            let findIt = false;
-            function search () {
-                const findInTitle = element.name.toLowerCase().includes(inputData);
-                const findInDescription = element.description.toLowerCase().includes(inputData);
-                let findInIngredients = false;
-
-                element.ingredients.forEach(element => {
-                    const see = element.ingredient.toLowerCase().includes(inputData);
-                    if (see == true) {
-                        findInIngredients = true
-                    }
-                });
-
-                if (findInTitle || findInDescription || findInIngredients == true) {
-                    findIt = true;
-                }
-                else {
-                    findIt = false;
-                }
-            }
-            search();
-
-            if (findIt == true) {
-                newTabRecipes.push(element);
+        const newTabRecipes = recipes.filter(element => {
+            const match = inputMatch (inputData, element);
+            if (match == true) {
+                return element;
             }
         });
         new Recipes (newTabRecipes);
@@ -39,4 +15,38 @@ export function algo (e) {
     else {
         new Recipes (recipes);
     }
+}
+//looking for a match
+function inputMatch (inputData, element) {
+    const findInTitle = element.name.toLowerCase().includes(inputData);
+    const findInDescription = element.description.toLowerCase().includes(inputData);
+    const findInIngredients = element.ingredients.some(element => {
+        return element.ingredient.toLowerCase().includes(inputData) === true;
+    });
+    if (findInTitle || findInDescription || findInIngredients == true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function filters () {
+
+}
+
+function findInTitle (element, data) {
+
+}
+
+function findInIngredients (element, data) {
+
+}
+
+function findInAppliances (element, data) {
+
+}
+
+function findInUstensils (element, data) {
+
 }
